9844d140e49a0f145ff0a742826a8cbca792efc0
Mda <102478989+sadlyOs@users.noreply.github.com>
2022-07-28 21:09:47 +0500

    Update bot.py

--- notes/management/commands/bot.py
+++ notes/management/commands/bot.py
@@ -3,13 +3,12 @@ from notes.models import Todo
 import sqlite3
 from pprint import pprint
 
-data = sqlite3.connect("db.sqlite3")
-
 
 class Command(BaseCommand):
     help = "Телеграм-бот"
 
     def handle(self, *args, **options):
-        cursor = data.cursor()
-        cursor.execute("""select * from notes_todo""")
-        pprint(cursor.fetchall())
+        with sqlite3.connect("db.sqlite3") as data:
+            cursor = data.cursor()
+            cursor.execute("""select * from notes_todo""")
+            pprint(cursor.fetchall())
